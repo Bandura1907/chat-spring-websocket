279be4129e937c02aa35313cadf48be0b91e0890
tarasbandura <taras190789@gmail.com>
2023-01-29 17:14:54 +0200

    update

--- src/main/java/com/example/websocket/WebsocketApplication.java
+++ src/main/java/com/example/websocket/WebsocketApplication.java
@@ -2,6 +2,9 @@ package com.example.websocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication
@@ -11,4 +14,15 @@ public class WebsocketApplication {
         SpringApplication.run(WebsocketApplication.class, args);
     }
 
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
 }
